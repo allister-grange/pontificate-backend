021459c064301ca7164e1723c067f836cc9d2f99
Allister Grange <allistergrange@gmail.com>
2020-12-30 18:08:13 +1300

    Basic socket.io connection working locally

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,40 @@
 import express from 'express';
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const index = require("./routes/index");
 
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+app.use(index);
+
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3005",
+    methods: ["GET", "POST"]
+  }
 });
 
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
