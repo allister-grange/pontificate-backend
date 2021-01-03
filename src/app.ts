f3ba06774a52ab567b42fac420db47a765c97ff3
Allister Grange <allistergrange@gmail.com>
2021-01-04 10:53:09 +1300

    Basic game lobby working

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 const http = require('http');
-const path = require('path');
-const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const index = require("./routes/index");
+const { getCurrentUser, userLeave, userJoin, getAllPlayersInGame } = require("./dummyuser");
 
 const app = express();
 app.use(index);
@@ -17,24 +16,55 @@ const io = require("socket.io")(server, {
   }
 });
 
-let interval;
+//can store players in an array for now 
+//https://github.com/RishabhVerma098/gameappBackend/blob/master/server.js
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
+io.on("connection", async (socket) => {
+
+  console.log("New connection");
+  
+
+  socket.on("playerLobbyEvent", () => {
+    //* create user
+    const { gameId, userName } = socket.handshake.query;
+    const user = userJoin(socket.id, userName, gameId);
+    socket.join(user.game);
+    const playersInGame = getAllPlayersInGame(gameId);
+    console.log('Game ' + gameId + ' had player ' + userName + ' join');
+
+    //* emit message to user to welcome them
+    socket.emit("playerLobbyEvent", {
+      userId: user.id,
+      userName: user.userName,
+      text: `Welcome ${user.userName}`,
+      playersInGame: playersInGame
+    });
+
+    //* Broadcast message to everyone except user that he has joined
+    socket.broadcast.to(user.game).emit("playerLobbyEvent", {
+      userId: user.id,
+      userName: user.userName,
+      text: `${user.userName} has joined the game`,
+      playersInGame: playersInGame
+    });
   });
-});
 
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
+  // Disconnect , when user leave room
+  socket.on("disconnect", () => {
+    // delete user from users & emit that user has left the game
+    const user = userLeave(socket.id);
+
+    if (user) {
+      console.log(`User ${user.userName} left the game`);
+      
+      io.to(user.room).emit("message", {
+        userId: user.id,
+        userName: user.userName,
+        text: `${user.userName} has left the game`,
+      });
+    }
+  });
+
+});
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ src/dummyuser.ts
@@ -0,0 +1,37 @@
+// TODO need to replace this with some sort of db
+const users = [];
+
+// Join user to chat
+function userJoin(id, userName, game) {
+  const user = { id, userName, game };
+
+  users.push(user);
+  console.log(users, "users");
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find((user) => user.id === id);
+}
+
+function getAllPlayersInGame(gameId: string): any[] {
+    return users.filter((user) => user.game === gameId);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+module.exports = {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getAllPlayersInGame
+};
